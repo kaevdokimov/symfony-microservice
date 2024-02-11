62263ac7793a963d9b17ce1f85adc2f3c9711e3d
kevdokimov <79169605859@ya.ru>
2024-02-12 01:23:39 +0300

    Add Modifier for date_range_multiplier type

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -9,8 +9,12 @@ class LowestPriceFilter implements PromotionsFilterInterface
 {
     public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotion): PromotionEnquiryInterface
     {
+        $price = $enquiry->getProduct()->getPrice();
+        $quantity = $enquiry->getQuantity();
+        $lowestPrice = $quantity * $price;
+        //$modifiedPrice = $priceModifier->modify($price, $quantity, $promotion, $enquiry);
         $enquiry->setPrice(100);
-        $enquiry->setDiscountedPrice(50);
+        $enquiry->setDiscountedPrice(250);
         $enquiry->setPromotionId(3);
         $enquiry->setPromotionName('Black Friday half price sale');
         return $enquiry;
