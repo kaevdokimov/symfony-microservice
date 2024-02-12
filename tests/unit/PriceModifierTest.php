04027d48b40d710b2c6da9730384256948af58d6
kevdokimov <79169605859@ya.ru>
2024-02-13 02:14:52 +0300

    Add test for date_range_multiplier type

--- tests/unit/PriceModifierTest.php
+++ tests/unit/PriceModifierTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\unit;
 use App\DTO\LowestPriceEnquiry;
 use App\Entity\Promotion;
 use App\Filter\Modifier\DateRangeMultiplier;
+use App\Filter\Modifier\EvenItemsMultiplier;
 use App\Filter\Modifier\FixedPriceVoucher;
 use App\Tests\ServiceTestCase;
 
@@ -51,5 +52,26 @@ class PriceModifierTest extends ServiceTestCase
         $modifiedPrice = $fixedPriceVoucher->modify(150, 5, $promotion, $enquiry);
         // Then
         $this->assertEquals(500, $modifiedPrice);
+    }
+
+    /** @test */
+    public function EvenItemsMultiplier_returns_a_correctly_modifier_price(): void
+    {
+        // Given
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+
+        $promotion = new Promotion();
+        $promotion->setName('Buy one get one free');
+        $promotion->setAdjustment(0.5);
+        $promotion->setCriteria(['minimum_quantity' => 2]);
+        $promotion->setType('date_range_multiplier');
+
+        $evenItemsMultiplier = new EvenItemsMultiplier();
+        // When
+        $modifiedPrice = $evenItemsMultiplier->modify(100, 5, $promotion, $enquiry);
+        // Then
+        // (0.5 * (100 * 4)) + (1 * 100)
+        $this->assertEquals(300, $modifiedPrice);
     }
 }
