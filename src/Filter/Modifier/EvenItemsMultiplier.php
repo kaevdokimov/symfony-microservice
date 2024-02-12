e30b5ae20be15ed1c0652396adddd2efa6dfb4fa
kevdokimov <79169605859@ya.ru>
2024-02-13 02:14:04 +0300

    Add Modifier for date_range_multiplier type

---
+++ src/Filter/Modifier/EvenItemsMultiplier.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filter\Modifier;
+
+use App\DTO\PromotionEnquiryInterface;
+use App\Entity\Promotion;
+
+class EvenItemsMultiplier implements PriceModifierInterface
+{
+    public function modify(int $price, int $quantity, Promotion $promotion, PromotionEnquiryInterface $enquiry): int
+    {
+        if($quantity < 2)
+            return $price * $quantity;
+
+        $oddCount = $quantity % 2; // 0 or 1
+        // Count how many even items
+        $evenCount = $quantity - $oddCount;
+
+        // ($adjustment * ($price * $evenCount)) + ($oddCount * $price)
+        return ($promotion->getAdjustment() * ($price * $evenCount)) + ($oddCount * $price);
+    }
+}
