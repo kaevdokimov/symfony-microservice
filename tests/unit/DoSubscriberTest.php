9330eb425acf4aa9dd01a5c94da76d253218e5f0
kevdokimov <79169605859@ya.ru>
2024-02-24 21:31:24 +0300

    Added test for check exists event

--- tests/unit/DoSubscriberTest.php
+++ tests/unit/DoSubscriberTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\unit;
 
 use App\DTO\LowestPriceEnquiry;
 use App\Event\AfterDtoCreatedEvent;
+use App\EventSubscriber\DtoSubscriber;
 use App\Service\ServiceException;
 use App\Tests\ServiceTestCase;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
@@ -11,6 +12,12 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class DoSubscriberTest extends ServiceTestCase
 {
+    /** @test */
+    public function testEventSubscription(): void
+    {
+        $this->assertArrayHasKey(AfterDtoCreatedEvent::NAME, DtoSubscriber::getSubscribedEvents());
+    }
+
     /** @test */
     public function tesValidateDto(): void
     {
